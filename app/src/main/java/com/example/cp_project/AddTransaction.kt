f3e6ffa592a972be1c0abca3ec6660aa07ec9afc
Hardik <hardikvaghasiya69@gmail.com>
2021-11-28 19:33:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/cp_project/AddTransaction.kt
@@ -0,0 +1,11 @@
+package com.example.cp_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddTransaction : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.add_transaction)
+    }
+}
